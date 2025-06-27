7de4e7d1af2e368df5544603f8b9b9a61b2e14bd
vivian1174 <85981415+vivian1174@users.noreply.github.com>
2025-06-27 10:17:41 +0800

    Update ngi_downloader_and_summary.py

--- ngi_downloader_and_summary.py
+++ ngi_downloader_and_summary.py
@@ -28,7 +28,7 @@ options.add_experimental_option("prefs", prefs)
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--window-size=1920,1080")
+options.add_argument("--window-size=1280,720")
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-blink-features=AutomationControlled")
 
